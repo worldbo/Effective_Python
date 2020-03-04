d711872ee24a8f96bc6451193030cd499c8ae7d3
worldbo <world_bo@sina.com>
2020-03-04 22:34:39 +0800

    第八条：在page18

--- chapter1/Helper_Function.py
+++ chapter1/Helper_Function.py
@@ -92,3 +92,38 @@ chile_len_set = {len(name) for name in rank_dict.values()}
 print(rank_dict)
 print(chile_len_set)
 
+#page16
+matrix = [[1,2,3],[4,5,6],[7,8,9]]
+flat = [x**2 for row in matrix for x in row]# from left(or row in matrix)to right(for x in row).
+print(flat)
+
+#page16
+squared = [[x**2 for x in row] for row in matrix]
+print(squared)
+
+my_lists = [
+    [[1,2,3],[4,5,6]],
+]
+flat = [x for sublist1 in my_lists
+        for sublist2 in sublist1
+        for x in sublist2]
+print(flat)
+
+flat = []
+for sublist1 in my_lists:
+    for sublist2 in sublist1:
+        flat.extend(sublist2)
+print(flat)
+
+a = [1,2,3,4,5,6,7,8,9,10]
+b = [x for x in a if x > 4 if x % 2 == 0]
+c = [x for x in a if x > 4 and x % 2 == 0]
+assert c == b  # assert断言语句
+print(b)
+
+#page17(尽量不要用这种方式)
+matrix = [[1,2,3],[4,5,6],[7,8,9]]
+filtered = [[x for x in row if x % 3 ==0]
+            for row in matrix if sum(row) >= 10]
+print(filtered)
+
