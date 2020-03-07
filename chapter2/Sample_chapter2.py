ec9c694a4b08c33f1b7ea372069e15ce3f8a11e6
worldbo <world_bo@sina.com>
2020-03-07 13:52:08 +0800

    第二章：在page35

--- chapter2/Sample_chapter2.py
+++ chapter2/Sample_chapter2.py
@@ -43,3 +43,58 @@ else:
     print('Result is %.1f' % result)
 
 #page30
+def sort_priority(values,group):
+    def helper(x): #闭包函数,x是numbers中各个元素
+        if x in group:
+            return (0,x)
+        return (1,x)
+    values.sort(key=helper)#闭包函数传给key参数
+
+numbers = [8,3,1,2,5,4,7,6]
+group = {2,3,5,7}
+sort_priority(numbers,group)
+print(numbers)
+
+
+def sort_priority2(numbers,group):
+    found = False#Scope：'sort_priority2_
+    def helper(x):
+        if x in group:
+            found = True #seem simple#Scope：'helper'-----Bad!
+            return (0,x)
+        return (1,x)
+    numbers.sort(key=helper)
+    return found
+
+found = sort_priority2(numbers,group)
+print('Found:',found)
+print(numbers)
+
+
+#page33
+def sort_priority3(numbers,group):
+    found = False#Scope：'sort_priority2_
+    def helper(x):
+        nonlocal found
+        if x in group:
+            found = True #seem simple#Scope：'helper'-----Bad!
+            return (0,x)
+        return (1,x)
+    numbers.sort(key=helper)
+    return found
+
+#page34
+class Sorter(object):
+    def __init__(self,group):
+        self.group = group
+        self.found = False
+    def __call__(self, x):
+        if x in self.group:
+            self.found = True
+            return (0,x)
+        return (1,x)
+
+
+sorter = Sorter(group)
+numbers.sort(key=sorter)
+assert sorter.found is True
