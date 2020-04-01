b6389394c7e9d080dfbe470f9da68f596996f5e7
worldbo <world_bo@sina.com>
2020-04-01 23:16:55 +0800

    #第二十一条：

--- chapter2/21.py
+++ chapter2/21.py
@@ -1,1 +1,68 @@
-#page49
+# page49
+
+def safe_division(number, divisor, ignore_overflow, ignore_zero_division):
+    try:
+        return number / divisor
+    except OverflowError:
+        if ignore_overflow:
+            return 0
+        else:
+            raise
+    except ZeroDivisionError:
+        if ignore_zero_division:
+            return float('inf')
+        else:
+            raise
+
+
+result = safe_division(1.0, 10 ** 500, True, False)
+print(result)
+
+result = safe_division(1.0, 0, False, True)
+print(result)
+
+
+###
+def safe_division_b(number, divisor, ignore_overflow=False, ignore_zero_division=False):
+    try:
+        return number / divisor
+    except OverflowError:
+        if ignore_overflow:
+            return 0
+        else:
+            raise
+    except ZeroDivisionError:
+        if ignore_zero_division:
+            return float('inf')
+        else:
+            raise
+
+
+result = safe_division_b(1.0, 10 ** 500, ignore_overflow=True)
+print(result)
+
+result = safe_division_b(1.0, 0, ignore_zero_division=True)
+print(result)
+
+
+####page51
+def safe_division_c(number, divisor, *, ignore_overflow=False, ignore_zero_division=False):
+    try:
+        return number / divisor
+    except OverflowError:
+        if ignore_overflow:
+            return 0
+        else:
+            raise
+    except ZeroDivisionError:
+        if ignore_zero_division:
+            return float('inf')
+        else:
+            raise
+
+
+result = safe_division_c(1.0, 10 ** 500, ignore_overflow=True)
+print(result)
+
+#result = safe_division_c(1.0, 0, False, True)  #TypeError: safe_division_c() takes 2 positional arguments but 4 were given
+#print(result)
